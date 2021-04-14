3491339cab549455acfc702932c9be22f73a7467
Fernando Matzdorf <fernando@rindecuentas.org>
2021-04-14 04:56:02 -0500

    Fixed source and sourceRun formats, fixed supplier party generation, added contract separation based on start date.

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const releaseObject = require('./lib/release.js');
 
 let items = {};
 let metadata = {
-    dataSource: [ {id: 'imss'} ]
+    dataSource: 'comprasimss',
+    dataSourceRun: 'comprasimss-' + Date.now()
 }
 
 console.time('duration');
@@ -29,15 +30,35 @@ process.stdin
 process.stdin.on('end', () => {
     Object.keys(items).map(key => {
         let item = items[key];
-        let release = releaseObject(key, item, metadata);
-        process.stdout.write( JSON.stringify(release) );
-        process.stdout.write( "\n" );
+        let contracts = splitByDate(item);
+        contracts.map( (c, i) => {
+            let id = (i >= 1)? key + '-' + i : key;
+            let release = releaseObject(id, c, metadata);
+            process.stdout.write( JSON.stringify(release) );
+            process.stdout.write( "\n" );
+        });
         delete item;
         delete release;
     });
 
     process.exit(0);
 });
+
+function splitByDate(rows) {
+    let dates = {};
+    rows.map(row => {
+        let dateIndex = row.fecha_inicio.replace(/-/g, '_');
+        if(!dates.hasOwnProperty(dateIndex)) {
+            dates[dateIndex] = [];
+            dates[dateIndex].push(row);
+        }
+        else dates[dateIndex].push(row);
+    });
+
+    let contracts = [];
+    Object.keys(dates).map(date => { contracts.push(dates[date]) });
+    return contracts;
+}
 
 function getItemID(obj) {
     let id = [];
@@ -54,29 +75,36 @@ function getItemID(obj) {
     if(num_pedido) presence += 'N';
     if(num_orden_compra) presence += 'O';
 
-    let delegacion = getDelegacionCode(obj.delegacion);
-    if(delegacion != '') id.push(delegacion);
+    // if(obj.hasOwnProperty('unidad_compradora')) {
+    //     let uc = getUCCode(obj.unidad_compradora);
+    //     if(uc != '') id.push(uc);
+    //     else console.log(obj.unidad_compradora);
+    // }
+    // else {
+        let delegacion = getDelegacionCode(obj.delegacion);
+        if(delegacion != '') id.push(delegacion);
+    // }
 
     switch(presence) {
         case 'PF':
-            if(num_procedimiento.length >= 3 && !num_procedimiento.match(/^[a-zA-Z]*$/) )
+            if(!num_procedimiento.match(/^[a-zA-Z]*$/) )
                 id.push(num_procedimiento);
             if(num_factura != num_procedimiento)
                 id.push(num_factura);
             break;
         case 'PC':
-            if(num_procedimiento.length >= 3 && !num_procedimiento.match(/^[a-zA-Z]*$/) )
+            if(!num_procedimiento.match(/^[a-zA-Z]*$/) )
                 id.push(num_procedimiento);
             if(num_contrato != num_procedimiento)
                 id.push(num_contrato);
             break;
         case 'P':
-            if(num_procedimiento.length >= 3 && !num_procedimiento.match(/^[a-zA-Z]*$/) )
+            if(!num_procedimiento.match(/^[a-zA-Z]*$/) )
                 id.push(num_procedimiento);
             else id.push(obj.id_ficha);
             break;
         case 'PNO':
-            if(num_procedimiento.length >= 3 && !num_procedimiento.match(/^[a-zA-Z]*$/) )
+            if(!num_procedimiento.match(/^[a-zA-Z]*$/) )
                 id.push(num_procedimiento);
             if(num_pedido != num_procedimiento)
                 id.push(num_pedido);
@@ -84,7 +112,7 @@ function getItemID(obj) {
                 id.push(num_orden_compra);
             break;
         case 'C':
-            if(num_contrato.length >= 3 && !num_contrato.match(/^[a-zA-Z]*$/) )
+            if(!num_contrato.match(/^[a-zA-Z]*$/) )
                 id.push(num_contrato);
             else id.push(obj.id_ficha);
             break;
@@ -94,6 +122,82 @@ function getItemID(obj) {
     }
 
     return id.join('-');
+}
+
+function getUCCode(string) {
+    switch(string) {
+        case "ALMACEN DELEGACIONAL EN EL D.F. ZONA SUR": return "050GYR025";
+        case "ALMACEN DELEGACIONAL EN EL D.F. ZONA NORTE": return "050GYR016";
+        case 'UMAE TRAUMA Y ORTO M.S. "DR. VICTORIO DE LA FUENTE NARVAEZ" (D.F. NORT': return "050GYR049";
+        case "UMAE HOSPITAL GENERAL C.M.N. LA RAZA": return "050GYR043";
+        case "UMAE ESPECIALIDADES C.M.N. SIGLO XXI": return "050GYR998";
+        case "UMAE ESPECIALIDADES C.M.N. LA RAZA": return "050GYR055";
+        case "UMAE ONCOLOGIA C.M.N. SIGLO XXI": return "050GYR051";
+        case "UMAE CARDIOLOGIA C.M.N. SIGLO XXI": return "050GYR057";
+        case "UMAE PEDIATRIA C.M.N. SIGLO XXI": return "050GYR067";
+        case "UMAE GINECO - OBSTETRICIA LA RAZA": return "050GYR050";
+        case "UMAE GINECO - OBSTETRICIA LA RAZA - FARMACIA": return "050GYR050";
+        case "UMAE GINECO - OBSTETRICIA No. 4 (D.F. SUR)": return "050GYR036";
+        case "ALMACEN DELEGACIONAL EN JALISCO": return "050GYR002";
+        case "UMAE ESPECIALIDADES JALISCO": return "050GYR020";
+        case "UMAE PEDIATRIA JALISCO": return "050GYR060";
+        case "UMAE GINECO - OBSTETRICIA JALISCO": return "050GYR079";
+        case "ALMACEN DELEGACIONAL EN SONORA": return "050GYR031";
+        case "UMAE ESPECIALIDADES SONORA": return "050GYR037";
+        case "ALMACEN DELEGACIONAL EN VERACRUZ NORTE": return "050GYR039";
+        case "ALMACEN DELEGACIONAL EN VERACRUZ PUERTO (EXT. BIENES TERAPEUTICOS)": return "050GYR014";
+        case "ALMACEN DELEGACIONAL EN VERACRUZ SUR": return "050GYR022";
+        case "UMAE ESPECIALIDADES VERACRUZ (NTE.)": return "050GYR014";
+        case "ALMACEN DELEGACIONAL EN BAJA CALIFORNIA NORTE": return "050GYR003";
+        case "ALMACEN DELEGACIONAL EN COAHUILA": return "050GYR026";
+        case "UMAE ESPECIALIDADES COAHUILA": return "050GYR045";
+        case "UMAE ESPECIALIDADES COAHUILA - SU-ALMACEN": return "050GYR045";
+        case "ALMACEN DELEGACIONAL EN NUEVO LEON": return "050GYR035";
+        case "H Gineco-Obstetricia 23 Felix Residencia De Conservacion-Res": return "050GYR088";
+        case "UMAE ESPECIALIDADES NUEVO LEON": return "050GYR059";
+        case "UMAE CARDIOLOGIA EN NUEVO LEON": return "050GYR076";
+        case "UMAE TRAUMATOLOGIA Y ORTOPEDIA NUEVO LEON": return "050GYR082";
+        case "UMAE GINECO - OBSTETRICIA NUEVO LEON": return "050GYR088";
+        case "ALMACEN DELEGACIONAL EN CHIHUAHUA": return "050GYR009";
+        case "ALMACEN DELEGACIONAL EN SINALOA": return "050GYR029";
+        case "ALMACEN DELEGACIONAL EN EL ESTADO DE MEXICO PONIENTE": return "050GYR024";
+        case "ALMACEN DELEGACIONAL EN ESTADO DE MEXICO ORIENTE": return "050GYR028";
+        case "UMAE TRAUMATOLOGIA Y ORTOPEDIA LOMAS VERDES": return "050GYR071";
+        case "UMAE TRAUMATOLOGIA Y ORTOPEDIA LOMAS VERDES - FARMACIA": return "050GYR071";
+        case "ALMACEN DELEGACIONAL EN GUANAJUATO": return "050GYR027";
+        case "UMAE ESPECIALIDADES GUANAJUATO": return "050GYR058";
+        case "UMAE GINECO - PEDIATRIA GUANAJUATO": return "050GYR074";
+        case "ALMACEN DELEGACIONAL EN PUEBLA": return "050GYR006";
+        case "UMAE ESPECIALIDADES PUEBLA": return "050GYR046";
+        case "UMAE TRAUMATOLOGIA Y ORTOPEDIA PUEBLA": return "050GYR091";
+        case "UMAE TRAUMATOLOGIA Y ORTOPEDIA PUEBLA - SUB-ALMACEN": return "050GYR091";
+        case "ALMACEN DELEGACIONAL EN OAXACA": return "050GYR013";
+        case "ALMACEN DELEGACIONAL EN TAMAULIPAS": return "050GYR018";
+        case "ALMACEN DELEGACIONAL EN SAN LUIS POTOSI": return "050GYR023";
+        case "ALMACEN DELEGACIONAL EN BAJA CALIFORNIA SUR": return "050GYR030";
+        case "Ofnas Centrales -Reforma- Coordinacion De Abastecimiento": return "050GYR047";
+        case "ALMACEN DE PROGRAMAS ESPECIALES Y RED FRIA": return "050GYR047B";
+        case "ALMACEN DELEGACIONAL EN DURANGO": return "050GYR010";
+        case "ALMACEN SUBDELEGACIONAL EN TAPACHULA, CHIAPAS": return "050GYR004";
+        case "ALMACEN SUBDELEGACIONAL EN TUXTLA GUTIERREZ, CHIAPAS": return "050GYR004";
+        case "ALMACEN DELEGACIONAL EN COLIMA": return "050GYR012";
+        case "ALMACEN DELEGACIONAL EN YUCATAN": return "050GYR011";
+        case "UMAE ESPECIALIDADES YUCATAN": return "050GYR063";
+        case "ALMACEN DELEGACIONAL EN MORELOS": return "050GYR007";
+        case "ALMACEN DELEGACIONAL EN QUINTANA ROO": return "050GYR008";
+        case "ALMACEN DELEGACIONAL EN MICHOACAN": return "050GYR033";
+        case "ALMACEN DELEGACIONAL EN GUERRERO": return "050GYR001";
+        case "ALMACEN DELEGACIONAL EN HIDALGO": return "050GYR017";
+        case "ALMACEN DELEGACIONAL EN QUERETARO": return "050GYR075";
+        case "ALMACEN DELEGACIONAL EN ZACATECAS": return "050GYR034";
+        case "ALMACEN DELEGACIONAL EN AGUASCALIENTES": return "050GYR032";
+        case "H Gral Zona 2 Almacen De Unidad Medica": return "050GYR032";
+        case "ALMACEN DELEGACIONAL EN CAMPECHE": return "050GYR069";
+        case "ALMACEN DELEGACIONAL EN TABASCO": return "050GYR015";
+        case "ALMACEN DELEGACIONAL EN NAYARIT": return "050GYR005";
+        case "ALMACEN DELEGACIONAL EN TLAXCALA": return "050GYR041";
+        default: return '';
+    }
 }
 
 function getDelegacionCode(string) {
